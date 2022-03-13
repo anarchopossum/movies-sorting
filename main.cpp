0f1ca57a80506f686418ded4f2b7d2341c237800
anarcopossum <92814289+anarcopossum@users.noreply.github.com>
2022-03-12 20:40:03 -0800

    added more sort function
    Need to fix issues with names becoming corrupted after a sort

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@ struct Movie {
 void populate (Movie *m[]);
 void sortByName(Movie *m[]);
 void sortByAct (Movie *m[]);
+void sortByRating (Movie *m[]);
+void sortByRunTime (Movie *m[]);
+
 
 void Menu(int a){
     cout << "1. Sort by Movie";
@@ -25,7 +28,10 @@ int main() {
     struct Movie *DVD[10];
     populate (DVD);
     sortByName(DVD);
-    sortByAct(DVD);
+    //sortByAct(DVD);
+    //sortByRating(DVD);
+    sortByRunTime(DVD);
+
 
 
     return 0;
@@ -49,76 +55,98 @@ void sortByAct (Movie *m[])
         }
     }
 }
+void sortByRating (Movie *m[])
+{ // this is bubble sort
+    for(int i = 0; i < 10-1; i++) {
+        for (int j = 0; j < 10 - i-1; j++) {
+            if (m[j]->rating > m[j + 1]->rating)  // check the code below
+                swap(m[j], m[j + 1]);
+        }
+    }
+}
+void sortByRunTime (Movie *m[])
+{ // this is bubble sort
+    for(int i = 0; i < 10-1; i++) {
+        for (int j = 0; j < 10 - i-1; j++) {
+            if (m[j]->runTime > m[j + 1]->runTime)  // check the code below
+                swap(m[j], m[j + 1]);
+        }
+    }
+}
+void sortByDate (Movie *m[])
+{
+
+}
 
 // Populates Struct with given values
 void populate (Movie *m[]){
     m[0] = new Movie;
     strncpy(m[0]->name,"Jaws",4); // can Memory Leak
-    m[0]->runTime = 5;
-    m[0]->rating  = 60;
-    //m[0]->release = (1,1,2000);
-    m[0]->actor   = "James Pants";
+    m[0]->runTime = 124;
+    m[0]->rating  = 8.10;
+    //m[0]->release = (6,20,1975);
+    m[0]->actor   = "Roy Scheider";
 
     m[1] = new Movie;
     strncpy(m[1]->name,"Shrek",5); // can Memory Leak
-    m[1]->runTime = 5;
-    m[1]->rating  = 60;
-    //m[0]->release = (1,1,2000);
-    m[1]->actor   = "James Pants";
+    m[1]->runTime = 90;
+    m[1]->rating  = 7.90;
+    //m[0]->release = (5,18,2001);
+    m[1]->actor   = "Mike Myers";
 
     m[2] = new Movie;
     strncpy(m[2]->name,"Rushhour",8); // can Memory Leak
-    m[2]->runTime = 5;
-    m[2]->rating  = 60;
-    //m[0]->release = (1,1,2000);
-    m[2]->actor   = "James Pants";
+    m[2]->runTime = 98;
+    m[2]->rating  = 7.0;
+    //m[0]->release = (9,18,1998);
+    m[2]->actor   = "Jackie Chan";
 
     m[3] = new Movie;
     strncpy(m[3]->name,"Bee Movie",9); // can Memory Leak
-    m[3]->runTime = 5;
-    m[3]->rating  = 60;
-    //m[0]->release = (1,1,2000);
-    m[3]->actor   = "James Pants";
+    m[3]->runTime = 91;
+    m[3]->rating  = 6.10;
+    //m[0]->release = (11,2,2007);
+    m[3]->actor   = "Jerry Seinfeld";
 
     m[4] = new Movie;
     strncpy(m[4]->name,"Shark Tale",10); // can Memory Leak
-    m[4]->runTime = 5;
-    m[4]->rating  = 60;
-    //m[0]->release = (1,1,2000);
-    m[4]->actor   = "James Pants";
+    m[4]->runTime = 90;
+    m[4]->rating  = 6.0;
+    //m[0]->release = (10,1,2004);
+    m[4]->actor   = "Will Smith";
 
     m[5] = new Movie;
     strncpy(m[5]->name,"Toy Story",9); // can Memory Leak
-    m[5]->runTime = 5;
-    m[5]->rating  = 60;
-    //m[0]->release = (1,1,2000);
-    m[5]->actor   = "James Pants";
+    m[5]->runTime = 81;
+    m[5]->rating  = 8.30;
+    //m[0]->release = (1,1,1995);
+    m[5]->actor   = "Tom Hanks";
 
     m[6] = new Movie;
     strncpy(m[6]->name,"The Iron Giant", 14); // can Memory Leak
     m[6]->runTime = 151;
-    m[6]->rating  = 8;
+    m[6]->rating  = 8.10;
     //m[0]->release = (8,6,1999);
-    m[6]->actor   = "Vin Diesel";
+    m[6]->actor   = "Eli Marienthal";
 
     m[7] = new Movie;
     strncpy(m[7]->name,"Up",2); // can Memory Leak
-    m[7]->runTime = 5;
-    m[7]->rating  = 60;
+    m[7]->runTime = 96;
+    m[7]->rating  = 8.70;
     //m[0]->release = (1,1,2000);
     m[7]->actor   = "Carl Fredericksen";
 
     m[8] = new Movie;
     strncpy(m[8]->name,"WALL-E",6); // can Memory Leak
     m[8]->runTime = 97;
-    m[8]->rating  = 60;
+    m[8]->rating  = 8.40;
     //m[0]->release = (6,27,2008);
     m[8]->actor   = "Ben Burtt";
 
     m[9] = new Movie;
     strncpy(m[9]->name,"Coco",4); // can Memory Leak
-    m[9]->runTime = 109;
-    m[9]->rating  = 60;
-    //m[0]->release = (1,1,2000);
+    m[9]->runTime = 105;
+    m[9]->rating  = 8.40;
+    //m[0]->release = (10,20,2017);
     m[9]->actor   = "Anthony Gonzalez";
 }
